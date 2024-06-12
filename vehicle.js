e545b653ac800dfe78f9fb15a8da388149fe76e8
mounir achir <mistamounir02@gmail.com>
2024-06-12 12:33:23 +0200

    commit

---
+++ vehicle.js
@@ -0,0 +1,17 @@
+// Implement the Car class that implements the Vehicle interface
+var Car = /** @class */ (function () {
+    function Car(make, model, year) {
+        this.make = make;
+        this.model = model;
+        this.year = year;
+    }
+    // Implement the start method
+    Car.prototype.start = function () {
+        console.log("Car engine started");
+    };
+    return Car;
+}());
+// Create an instance of the Car class
+var myCar = new Car("Toyota", "Camry", 2022);
+// Call the start method on the instance of the Car class
+myCar.start();
